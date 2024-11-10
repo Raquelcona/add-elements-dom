76fb78d9a3c81696c86e6f1d8633d7f5128bed60
Raquelcona <raquelcolladosnavarro@gmail.com>
2024-11-10 16:23:22 +0100

    actualizacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
  // Aquí tu código
+const lista = document.getElementById("Lista");
+const boton = document.getElementById("agregar");
+
+function agregarElemento() {
+    const nuevoElemento = prompt ("introduce un elemento para agregar a la lista");
+
+    const li = document.createElement("li");
+    li.textContent = nuevoElemento;
+    lista.appendChild(li);
+}
+
+boton.addEventListener("click", agregarElemento);
+
